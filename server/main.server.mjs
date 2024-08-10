569bacc3557aff0dc87d406af501646d7f4abf28
AkshayK <akshayakhi456@gmail.com>
2024-08-10 22:24:43 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4X7NICEW.mjs";import"./chunk-YPUUCF7V.mjs";import"./chunk-IWOKQOH3.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-RGNWAOKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
